af8f7b4f5f419f42f85fc44b0312f59373665ec0
fujima3 <fujimasa@positrium.org>
2021-07-01 09:45:22 +0900

    add tampermonkey main.js.

--- src/main/ts/Main.ts
+++ src/main/ts/Main.ts
@@ -1,3 +1,24 @@
-export class Main {
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
 
+(function() {
+  'use strict';
+
+  // Your code here...
+  new Main().run();
+
+})();
+
+export class Main {
+  public run(){
+
+  }
 }
